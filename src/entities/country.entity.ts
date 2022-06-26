ce156b4d994da239df08919dcd2b6c722678e84f
David Delgado <delgadoch.david@gmail.com>
2022-06-25 21:36:48 -0400

    feat: update contry entity name

--- src/entities/country.entity.ts
+++ src/entities/country.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity({ name: 'countries' })
 export class Country {
   @PrimaryGeneratedColumn()
   public id!: number;
